eb1118858dcec04c63017dbb587e53af59f3d37e
ssathish1992 <ssathish1992@gmail.com>
2026-03-29 20:33:04 +0530

    mail fix

--- src/test/java/com/w2a/APITestingFramework/utilities/TestConfig.java
+++ src/test/java/com/w2a/APITestingFramework/utilities/TestConfig.java
@@ -1,39 +1,41 @@
 package com.w2a.APITestingFramework.utilities;
-public class TestConfig{
 
+import com.w2a.APITestingFramework.setUp.BaseTest;
 
-	
-	public static String server="smtp.gmail.com";
-	public static String from = "corporate@way2automation.com";
-	public static String password = "Selenium@123";
-	public static String[] to ={"seleniumcoaching@gmail.com","trainer@way2automation.com"};
-	public static String subject = "Extent Project Report";
-	
-	public static String messageBody ="TestMessage";
-	/*public static String attachmentPath="c:\\screenshot\\2017_10_3_14_49_9.jpg";
-	public static String attachmentName="error.jpg";*/
-	
-	
-	
-	//SQL DATABASE DETAILS	
-	public static String driver="net.sourceforge.jtds.jdbc.Driver"; 
-	public static String dbConnectionUrl="jdbc:jtds:sqlserver://192.101.44.22;DatabaseName=monitor_eval"; 
-	public static String dbUserName="sa"; 
-	public static String dbPassword="$ql$!!1"; 
-	
-	
-	//MYSQL DATABASE DETAILS
-	public static String mysqldriver="com.mysql.jdbc.Driver";
-	public static String mysqluserName = "root";
-	public static String mysqlpassword = "selenium";
-	public static String mysqlurl = "jdbc:mysql://localhost:3306/acs";
-	
-	
-	
-	
-	
-	
-	
-	
-	
+public class TestConfig {
+
+    // MAIL SERVER DETAILS
+    public static String server = "smtp.gmail.com";
+    
+    // We use BaseTest.getProperty to allow Jenkins to override these at runtime
+    public static String from = getSetting("from", "corporate@way2automation.com");
+    public static String password = getSetting("password", "Selenium@123");
+    
+    public static String[] to = {"ssathish1992@gmail.com"};
+    public static String subject = "Extent Project Report";
+    public static String messageBody = "TestMessage";
+    
+    // SQL DATABASE DETAILS
+    public static String driver = "net.sourceforge.jtds.jdbc.Driver"; 
+    public static String dbConnectionUrl = "jdbc:jtds:sqlserver://192.101.44.22;DatabaseName=monitor_eval"; 
+    public static String dbUserName = "sa"; 
+    public static String dbPassword = "$ql$!!1"; 
+    
+    // MYSQL DATABASE DETAILS
+    public static String mysqldriver = "com.mysql.jdbc.Driver";
+    public static String mysqluserName = "root";
+    public static String mysqlpassword = "selenium";
+    public static String mysqlurl = "jdbc:mysql://localhost:3306/acs";
+
+    /**
+     * Helper method to check System properties (Jenkins) first, 
+     * then fall back to the hardcoded default.
+     */
+    private static String getSetting(String key, String defaultValue) {
+        String value = System.getProperty(key);
+        if (value == null || value.isEmpty() || value.contains("${")) {
+            return defaultValue;
+        }
+        return value;
+    }
 }
